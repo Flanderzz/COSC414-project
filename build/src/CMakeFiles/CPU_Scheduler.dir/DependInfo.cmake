82b5a0447b054dacaece59f5b0ce1459d21bb061
Ned Flanderzz <devinechinmere@gmail.com>
2024-12-05 20:08:24 -0500

    initial commit

---
+++ build/src/CMakeFiles/CPU_Scheduler.dir/DependInfo.cmake
@@ -0,0 +1,26 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/Users/devinechinemere/Documents/COSC414-project/src/Process.cpp" "src/CMakeFiles/CPU_Scheduler.dir/Process.cpp.o" "gcc" "src/CMakeFiles/CPU_Scheduler.dir/Process.cpp.o.d"
+  "/Users/devinechinemere/Documents/COSC414-project/src/RoundRobin.cpp" "src/CMakeFiles/CPU_Scheduler.dir/RoundRobin.cpp.o" "gcc" "src/CMakeFiles/CPU_Scheduler.dir/RoundRobin.cpp.o.d"
+  "/Users/devinechinemere/Documents/COSC414-project/src/Scheduler.cpp" "src/CMakeFiles/CPU_Scheduler.dir/Scheduler.cpp.o" "gcc" "src/CMakeFiles/CPU_Scheduler.dir/Scheduler.cpp.o.d"
+  "/Users/devinechinemere/Documents/COSC414-project/src/main.cpp" "src/CMakeFiles/CPU_Scheduler.dir/main.cpp.o" "gcc" "src/CMakeFiles/CPU_Scheduler.dir/main.cpp.o.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ build/src/CMakeFiles/CPU_Scheduler.dir/cmake_clean.cmake
@@ -0,0 +1,17 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/CPU_Scheduler.dir/Process.cpp.o"
+  "CMakeFiles/CPU_Scheduler.dir/Process.cpp.o.d"
+  "CMakeFiles/CPU_Scheduler.dir/RoundRobin.cpp.o"
+  "CMakeFiles/CPU_Scheduler.dir/RoundRobin.cpp.o.d"
+  "CMakeFiles/CPU_Scheduler.dir/Scheduler.cpp.o"
+  "CMakeFiles/CPU_Scheduler.dir/Scheduler.cpp.o.d"
+  "CMakeFiles/CPU_Scheduler.dir/main.cpp.o"
+  "CMakeFiles/CPU_Scheduler.dir/main.cpp.o.d"
+  "CPU_Scheduler"
+  "CPU_Scheduler.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/CPU_Scheduler.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
